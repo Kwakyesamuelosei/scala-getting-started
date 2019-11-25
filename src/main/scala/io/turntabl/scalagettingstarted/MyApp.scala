48a80708acd290073c9a1defc40382632d7cfc49
Samuel Osei Kwakye <kwakyesamuelosei@gmail.com>
2019-11-25 11:41:18 +0000

    My First Scala App

--- src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
+++ src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
@@ -1,5 +1,41 @@
 package io.turntabl.scalagettingstarted
 
 object MyApp extends App {
-    print("Hello, World!")
+    val firstName = "Sam"
+    val homeTown = "Kwahu"
+//    print(s"$firstName was born in $homeTown \n");
+
+    def threeTimes(word: String) = word * 3
+//    print(threeTimes("Yaa"))
+
+    def even(num: Int) = if (num % 2 == 0) true else false
+//    print(even(3))
+
+    def odd(num: Int) = if (even(num)) false else true
+//    print(odd(2))
+
+    def wordsToLower(words: String*) = words map (_.toLowerCase)
+//    print(wordsToLower("NADIA","TAWIAH","UG"))
+
+    def isWeekendDay(day: String) =
+        day match {
+            case "Friday" => true
+            case "Saturday" => true
+            case "Sunday" => true
+            case _ => false
+        }
+//    print(isWeekendDay("Friday"))
+    def areWeekendDays(days: String*) =
+        days map (isWeekendDay(_))
+//    print(areWeekendDays("Monday","Saturday","Sunday"))
+
+//    areWeekendDays("Monday","Saturday","Sunday") foreach( println)
+
+    def mutiple(name: String, num: Int) =
+        name * num
+//    print(mutiple("Doreen",4))
+
+
+
 }
+
